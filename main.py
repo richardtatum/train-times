5a34bfab835c20e2de4e5f1645855b58b8f09159
richardtatum <DP@DP.com>
2019-07-01 15:04:04 +0100

    Basic commenting

--- main.py
+++ main.py
@@ -14,20 +14,24 @@ def show_departures(dep, dest_code):
         'app_key': os.getenv('app_key'),
         'calling_at': dest_code,
         'station_detail': 'calling_at',
-        'from_offset': '-PT00:30:00',
-        'to_offset': 'PT02:00:00'
+        'from_offset': '-PT00:30:00',  # From 30mins in the past
+        'to_offset': 'PT02:00:00'  # To 2 hours in the future
     }
     r = requests.get(url=URL, params=PARAMS)
     data = r.json()
 
     if 'error' in data:
         raise ValueError(data['error'])
+    
+    # Full station names for departure and destination
     dep_name = data['station_name']
     dest_name = data['departures']['all'][0]['station_detail']['calling_at'][0]['station_name']
 
     format_data(dep_name, dest_name, dest_code, data['departures']['all'])
 
 
+# Extracts the required data from the JSON and formats it into a list of dicts
+# One for each departing train.
 def format_data(dep_name, dest_name, dest_code, data):
     departures = []
     for x in data:
@@ -43,6 +47,8 @@ def format_data(dep_name, dest_name, dest_code, data):
     format_email(departures, dep_name, dest_name)
 
 
+# Basic email formatting. Writes a header into the top, listing the departure
+# and arrival, then followed by the relevant train data
 def format_email(departures, dep_name, dest_name):
     with open(f'{DIR}/templates/email.html', 'w') as f:
         header = f'''
@@ -64,6 +70,8 @@ def format_email(departures, dep_name, dest_name):
     send_timings_email()
 
 
+# Get arrival time is called for a more accurate ETA into the requested
+# station as the standard ETA provided doesn't update when the train is late.
 def get_arrival_time(id, dest):
     r = requests.get(id)
     data = r.json()
